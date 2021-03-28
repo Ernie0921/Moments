2101a9b139feca90ed59dc35ffd9dad2f32cc5ec
Ernesto Carrillo <ernesto.carrillo10@icloud.com>
2021-03-28 00:17:19 -0400

    created client folder with react, rendered the first div in the browser

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+ import React from 'react'; //import react
+ import ReactDOM from 'react-dom';  //DOM for react
+
+ import App from './App'; //import the app function 
+
+ ReactDOM.render(<App />, document.getElementById('root'))  //render to the DOM the App variable that will createt the div h1 app, with in a div with an id "root"
