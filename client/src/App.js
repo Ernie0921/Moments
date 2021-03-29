f0ee45b1b3fb94be2d9377e5f8caad7d0404429b
Ernesto Carrillo <ernesto.carrillo10@icloud.com>
2021-03-28 20:20:06 -0400

    added componets

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'; //importing components 
+//Container = container 
+//AppBar is similar to nav bar
+//Typography  - used for h1-h6 or p tags
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import moments from './images/moments.png';
+
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center" >Moments</Typography>
+                <img src={ moments } alt="moments" height="80"/>
+            </AppBar>
+            <Grow in> 
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        
+        </Container>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
  import React from 'react'; //import react
  import ReactDOM from 'react-dom';  //DOM for react
 
- import App from './App'; //import the app function 
+ import App from './App'; //import the app function  from App.js file
 
  ReactDOM.render(<App />, document.getElementById('root'))  //render to the DOM the App variable that will createt the div h1 app, with in a div with an id "root"
