71c25a3ae3a061e7d165455dfd2b033ae51cea50
Ernesto Carrillo <ernesto.carrillo10@icloud.com>
2021-03-30 17:29:09 -0400

    stbackend is complete fro operations

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
 //WILL HANDLE ALL THE POST THAT HAVE TO DO WITH CORS
 
 import express from 'express'; 
-import { getPosts, createPost, updatePost, deletePost } from '../controllers/render.js'; // IMPORTED the getPosts variable from render.js
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/render.js'; // IMPORTED the getPosts variable from render.js
 
 const router = express.Router() //initialize the being able to create routes with this router 
 
@@ -11,4 +11,5 @@ router.get('/', getPosts );  // this route will run the function in the getPost 
 router.post('/', createPost)  // this route will run the createPost function hat was imported from render.js
 router.patch('/:id', updatePost)//updating 
 router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost)
 export default router; 
