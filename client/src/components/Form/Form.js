c1cdfcaaa2237ecb0ead1de54932156bf055dd12
Ernesto Carrillo <ernesto.carrillo10@icloud.com>
2021-03-30 23:01:58 -0400

    completed client side and deployed with netify

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -19,28 +19,30 @@ const Form = ({ currentId, setCurrentId}) => {
     const [postData, setPostData] = useState({creator: '', title: '', message: '', tags: '', selectedFile: ''});
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
     const classes = useStyles();
-
     const dispatch = useDispatch(); 
 
     useEffect(() => {
         if(post) setPostData(post);
-    }, [post])
-
-    const handleSubmit = (e) => { 
-        e.preventDefault();
-
-        if(currentId) {
-            dispatch(updatePost( currentId, postData));
-        } else {
-            dispatch(createPost(postData))
-        }
-
-       
-    }
+    }, [post]);
 
     const clear = () => {
+        setCurrentId(0);
+        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    };
 
-    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        if (currentId === 0) {
+          dispatch(createPost(postData));
+          clear();
+        } else {
+          dispatch(updatePost(currentId, postData));
+          clear();
+        }
+      };
+
+
 
     return (
         <Paper className={classes.paper}>
@@ -67,13 +69,15 @@ const Form = ({ currentId, setCurrentId}) => {
                     variant="outlined" 
                     label="Message" 
                     fullWidth
+                    multiline
+                    rows={4}
                     value={postData.message }
                     onChange={(e) => setPostData({ ...postData, message: e.target.value})}
                 />
                 <TextField 
                     name="tags" 
                     variant="outlined" 
-                    label="Tags" 
+                    label="Tags "
                     fullWidth
                     value={postData.tags }
                     onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',')})}
@@ -86,7 +90,7 @@ const Form = ({ currentId, setCurrentId}) => {
                     />
                 </div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
-                <Button variant="contained" color="secondary" size="small"  onClick={clear} type="submit" fullWidth>Clear</Button>
+                <Button variant="contained" color="secondary" size="small"  onClick={clear} fullWidth>Clear</Button>
             </form>
         
         </Paper>
